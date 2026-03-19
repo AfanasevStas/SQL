430b80119fea2204a19b12f8e1ccb22163e63d3b
AfanasevStas <afanasevstas96@mail.ru>
2026-03-19 21:49:21 +0300

    Bitwise operator

---
+++ Functions/SQLQuery1 - GetNextLearningDate.sql
@@ -0,0 +1,9 @@
+﻿--SQLQuery1 - GetNextLearningDate - возвращает дату проведения следующего занятия
+USE PV_522_Import
+GO
+
+CREATE FUNCTION GetNextLearningDate(@group_name AS NCHAR(10), @last_learning_date AS DATE = N'1900-01-01')RETURNS DATE
+AS
+BEGIN
+	
+END
